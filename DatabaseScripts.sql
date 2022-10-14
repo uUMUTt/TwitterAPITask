3de2147db98bee88e3620b747511a5e35d4d5b51
Umut Karadas <81423078+uUMUTt@users.noreply.github.com>
2022-10-14 23:22:56 +0300

    Update DatabaseScripts.sql

--- DatabaseScripts.sql
+++ DatabaseScripts.sql
@@ -2,10 +2,10 @@ CREATE DATABASE twitterdb;
  
 
 CREATE TABLE tweets (
- 	  tweet_id VARCHAR(255) PRIMARY KEY,
+ 	   tweet_id VARCHAR(255) PRIMARY KEY,
   	  username VARCHAR(255) NOT NULL,
- 	  content TEXT NOT NULL,
-        url VARCHAR(255) UNIQUE NOT NULL
+ 	   content TEXT NOT NULL,
+     url VARCHAR(255) UNIQUE NOT NULL
  );
 
 
